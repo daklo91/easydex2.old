813cf9fca69dd2d84514589289f2357201609b8e
d4kl0 <coffeedog91@gmail.com>
2020-10-05 21:28:04 +0200

    Fix Search

--- src/views/PokemonDisplay.vue
+++ src/views/PokemonDisplay.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <h1>Pokemon Display</h1>
-
-    <div v-for="(pokemon, index) in pokemons" :key="index">
-      <p>#{{ pokemon.id }} {{ pokemon.name }}</p>
-      <img :src="pokemon.sprites.front_default" alt="" />
+    <div>
+      <div v-for="(pokemon, index) in pokemons" :key="index">
+        <p>#{{ pokemon.id }} {{ pokemon.name }}</p>
+        <img
+          :src="pokemon.sprites.front_default"
+          :alt="'Image of ' + pokemon.name"
+        />
+      </div>
     </div>
 
     <PokemonItem />
   </div>
 </template>
-
 <script>
 import PokemonItem from '@/components/PokemonItem.vue'
 
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,11 +1,62 @@
 <template>
   <div>
-    <h1>Search</h1>
+    <h1 v-if="!searchOutput">Name or Index</h1>
+    <input
+      v-if="!searchOutput"
+      type="text"
+      v-model.number="searchInput"
+      v-on:keyup.enter="searchPokemon"
+    />
+    <button v-if="!searchOutput" @click.prevent="searchPokemon">Search</button>
+    <div v-if="searchOutput">
+      <p>
+        #{{ searchOutput.id }}
+        {{ searchOutput.name }}
+      </p>
+      <img
+        v-if="searchOutput"
+        :src="searchOutput.sprites.front_default"
+        :alt="'Image of ' + searchOutput.name"
+      />
+    </div>
+    <button v-if="searchOutput" @click.prevent="reload">
+      Reload
+    </button>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'Search',
+  data() {
+    return {
+      searchInput: null,
+      searchOutput: null
+    }
+  },
+  computed: {
+    pokemons() {
+      return this.$store.state.pokemonArray
+    }
+  },
+  methods: {
+    searchPokemon() {
+      if (typeof this.searchInput === 'number') {
+        this.searchOutput = this.pokemons.find(
+          pokemon => pokemon.id == this.searchInput
+        )
+      } else {
+        this.searchOutput = this.pokemons.find(
+          pokemon => pokemon.name === this.searchInput
+        )
+      }
+    },
+    reload() {
+      this.searchOutput = null
+      this.searchInput = null
+    }
+  }
+}
 </script>
 
 <style scoped></style>
