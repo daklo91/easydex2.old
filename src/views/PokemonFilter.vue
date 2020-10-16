aba9d68d5a7ed479ad38fc80837e76cbda28ae8b
daklo91 <dennis.bjorneset@gmail.com>
2020-10-16 15:37:37 +0200

    Add Style to PokemonFilter

--- src/views/PokemonFilter.vue
+++ src/views/PokemonFilter.vue
@@ -1,15 +1,36 @@
 <template>
   <div>
     <!-- TYPE Options -->
-    <div>
-      <div v-for="(type, index) in types" :key="index">
-        <button @click.prevent="pushTypeToFilter(type)">{{ type }}</button>
+    <div class="flex flex-row justify-center">
+      <div class="flex flex-col items-center mx-2">
+        <div v-for="type in types" :key="type">
+          <button
+            :style="{ background: pokemonColorByType(type) + 90 }"
+            class="main-font text-gray-800 p-2 m-1 rounded-3xl"
+            @click.prevent="pushTypeToFilter(type)"
+          >
+            {{ type }}
+          </button>
+        </div>
+      </div>
+      <div class="flex flex-col items-center mx-2">
+        <div v-for="type in types2" :key="type">
+          <button
+            :style="{ background: pokemonColorByType(type) + 90 }"
+            class="main-font text-gray-800 p-2 m-1 rounded-3xl"
+            @click.prevent="pushTypeToFilter(type)"
+          >
+            {{ type }}
+          </button>
+        </div>
       </div>
     </div>
     <p>{{ filterType }}</p>
-
+    <br />
+    <hr />
+    <br />
     <!-- WEIGHT Options -->
-    <div>
+    <div class="flex flex-row">
       <button @click="weightLow = !weightLow">
         Low weight: {{ weightLow }}
       </button>
@@ -20,9 +41,11 @@
         High Weight: {{ weightHigh }}
       </button>
     </div>
-
+    <br />
+    <hr />
+    <br />
     <!-- HEIGHT Options -->
-    <div>
+    <div class="flex flex-row">
       <button @click="heightLow = !heightLow">
         Low Height: {{ heightLow }}
       </button>
@@ -33,7 +56,9 @@
         High Height: {{ heightHigh }}
       </button>
     </div>
-
+    <br />
+    <hr />
+    <br />
     <!-- INDEX Options -->
     <div>
       <p>Index:</p>
@@ -68,7 +93,9 @@ export default {
         'Fighting',
         'Fire',
         'Flying',
-        'Ghost',
+        'Ghost'
+      ],
+      types2: [
         'Grass',
         'Ground',
         'Ice',
@@ -209,6 +236,48 @@ export default {
       if (this.filterType.length === 0) {
         this.filterType = ''
       }
+    },
+    pokemonColorByType(type) {
+      var color = null
+      type = type.toLowerCase()
+      if (type === 'bug') {
+        color = '#a8b820'
+      } else if (type === 'dark') {
+        color = '#705848'
+      } else if (type === 'dragon') {
+        color = '#7038f8'
+      } else if (type === 'electric') {
+        color = '#f8d030'
+      } else if (type === 'fairy') {
+        color = '#ffa0c2'
+      } else if (type === 'fighting') {
+        color = '#c03028'
+      } else if (type === 'fire') {
+        color = '#f08030'
+      } else if (type === 'flying') {
+        color = '#a890f0'
+      } else if (type === 'ghost') {
+        color = '#705898'
+      } else if (type === 'grass') {
+        color = '#78c850'
+      } else if (type === 'ground') {
+        color = '#e0c068'
+      } else if (type === 'ice') {
+        color = '#98d8d8'
+      } else if (type === 'normal') {
+        color = '#a8a878'
+      } else if (type === 'poison') {
+        color = '#a040a0'
+      } else if (type === 'psychic') {
+        color = '#f85888'
+      } else if (type === 'rock') {
+        color = '#b8a038'
+      } else if (type === 'steel') {
+        color = '#7b8e8a'
+      } else if (type === 'water') {
+        color = '#6890f0'
+      }
+      return color
     }
   }
 }
